eb371cf24969d97bf77ee47d586ddbaae04b7705
Furlukin <erwan.ansquer@gmail.com>
2025-02-23 23:25:24 +0100

    Adding custom lily58 keymap

---
+++ keyboards/furlukin/lily58/keymaps/default/rules.mk
@@ -0,0 +1,7 @@
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+LTO_ENABLE = yes 	    # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
